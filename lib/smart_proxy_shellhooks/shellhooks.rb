0ab8fd9fe13da830a7a40e9ed0025b960154ec50
Lukas Zapletal <lzap+git@redhat.com>
2021-03-08 17:49:59 +0100

    Do not listen on HTTP by default

--- lib/smart_proxy_shellhooks/shellhooks.rb
+++ lib/smart_proxy_shellhooks/shellhooks.rb
@@ -1,12 +1,13 @@
 module Proxy::ShellHooks
-  #class NotFound < RuntimeError; end
-
   class Plugin < ::Proxy::Plugin
     plugin 'shellhooks', Proxy::ShellHooks::VERSION
 
     default_settings :directory => '/var/lib/foreman/shellhooks'
 
-    http_rackup_path File.expand_path('shellhooks_http_config.ru', File.expand_path('../', __FILE__))
+    # Listening via HTTP is only good for development
+    if ENV["FOREMAN_SHELLHOOKS_HTTP"]
+      http_rackup_path File.expand_path('shellhooks_http_config.ru', File.expand_path('../', __FILE__))
+    end
     https_rackup_path File.expand_path('shellhooks_http_config.ru', File.expand_path('../', __FILE__))
   end
 end
